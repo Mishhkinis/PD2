afec1bff7bf97895e03fd96fd75e1f09f0087a66
Mishhkinis <adrijusanukas@gmail.com>
2026-03-27 08:36:14 +0200

    Kintamieji

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    string vardai[100];
+    int pazymiai[100][10];
+    int kiek[100]; // Kiek kiekvienas mokinys turi pazymiu (pvz. vienas 3, kitas 10)
+    int n = 0; // Mokiniu skaicius
+    int pasirinkimas;
     return 0;
 }
