57f7ea65d095f7f37b41edd3df79bfa47b5de93b
Mishhkinis <adrijusanukas@gmail.com>
2026-03-27 08:39:51 +0200

    Pasirinkimai

--- main.cpp
+++ main.cpp
@@ -2,12 +2,18 @@
 #include <string>
 
 using namespace std;
-
 int main() {
     string vardai[100];
     int pazymiai[100][10];
-    int kiek[100]; // Kiek kiekvienas mokinys turi pazymiu (pvz. vienas 3, kitas 10)
-    int n = 0; // Mokiniu skaicius
+    int n = 0; // Dabartinis mokiniø skaièius
     int pasirinkimas;
+
+    while (true) {
+        cout << "\n1. Prideti  2. Rodyti  3. Keisti  4. Trinti  0. Baigti\n";
+        cin >> pasirinkimas;
+        if (pasirinkimas == 0) break;
+
+        // Cia bus pildomos funkcijos
+    }
     return 0;
 }
